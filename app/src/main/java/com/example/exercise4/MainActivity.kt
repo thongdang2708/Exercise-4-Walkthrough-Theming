fe06109899c042bcc3744076a9aa89d8c78940e0
thongdang2708 <duythong2708@gmail.com>
2023-01-22 21:13:45 +0200

    Submit

---
+++ app/src/main/java/com/example/exercise4/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.exercise4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.exercise4.ui.theme.Exercise4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Exercise4Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MyApp();
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp () {
+    val appModifier = Modifier.fillMaxWidth().padding(8.dp)
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "My Title",
+            style = MaterialTheme.typography.h5,
+            modifier = appModifier
+        )
+        OutlinedTextField(value = "", onValueChange = {}, modifier = appModifier)
+        Button(onClick = { /*TODO*/ }, modifier = appModifier) {
+            Text(text = "submit")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Exercise4Theme {
+        MyApp()
+    }
+}
